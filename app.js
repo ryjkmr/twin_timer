a671a7b7e7dadd96faef757289e2c875240734a8
ryjkmr <kmr1@me.com>
2020-03-31 01:03:46 +0900

    fixed bug again

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const INTERVAL = 10 //インターバル設定。本番では1000=1秒に設定
-let loop;//= document.getElementById('loop').value;
-let timer1;//= document.getElementById('timer1').value;
-let timer2;//= document.getElementById('timer2').value;
+let loop;
+let timer1;
+let timer2;
 
 const loopDisplay = document.getElementById('loopDisp');
 const timer1Display = document.getElementById('timer1Disp');
@@ -12,10 +12,7 @@ const timer2Display = document.getElementById('timer2Disp');
 const startBtn = document.getElementById('start');
 startBtn.addEventListener('click', startTimer, false);
 const stopBtn = document.getElementById('stop');
-stopBtn.addEventListener('click', (event) => { 
-  clearInterval(timerId);
-  startBtn.disabled = "";
- });
+stopBtn.addEventListener('click', stopTimer, false);
 
 let timerId;
 
@@ -31,7 +28,7 @@ function startTimer() {
 }
 
 function countDownTimer1() {
-  timer1 -= 1;
+  timer1--;
   timer1Display.innerText = formatTime(timer1);
   if (timer1 <= 0) {
     clearInterval(timerId);
@@ -40,7 +37,7 @@ function countDownTimer1() {
 }
 
 function countDownTimer2() {
-  timer2 -= 1;
+  timer2--;
   timer2Display.innerText = formatTime(timer2);
   if (timer2 <= 0) {
     clearInterval(timerId);
@@ -49,13 +46,15 @@ function countDownTimer2() {
 }
 
 function countDownLoop() {
-  loop -= 1;
+  loop--;
   loopDisplay.innerText = loop;
   if (loop <= 0) {
     loopDisplay.innerHTML = "<H1>ALL DONE!</H1>";
-    clearInterval(timerId);
+    stopTimer();
   } else {
-    startTimer();
+    timer1 = document.getElementById('timer1').value * 60;
+    timer2 = document.getElementById('timer2').value * 60;
+    timerId = setInterval(countDownTimer1, INTERVAL);
   }
 }
 
@@ -63,4 +62,10 @@ function formatTime(time) { //[分:秒]表示のケタを揃える関数
   const min = Math.floor(time / 60).toString().padStart(2, '0');
   const sec = (time % 60).toString().padStart(2, '0');
   return `${min}:${sec}`;
+}
+
+function stopTimer() {
+  clearInterval(timerId);
+  startBtn.disabled = "";
+  loop = document.getElementById('loop').value;
 }
